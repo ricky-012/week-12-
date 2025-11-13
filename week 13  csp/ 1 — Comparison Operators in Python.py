d37db02c64a5a11b0ec0be99b7a47b4c17ad4be7
ricky-012 <ejherrera289@gmail.com>
2025-11-13 18:43:49 +0000

    Test

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -10,32 +10,38 @@
 
 # Remember: = is assignment, while == is comparison.
 
-a = 3
-b = 4
+# a = 3
+# b = 4
 
-print(a == b)   # False
-print(a != b)   # True
-print(a > b)    # False
-print(a < b)    # True
-print(a >= b)   # False
-print(a <= b)   # True
+# print(a == b)   # False
+# print(a != b)   # True
+# print(a > b)    # False
+# print(a < b)    # True
+# print(a >= b)   # False
+# print(a <= b)   # True
 
 
-#predict the output of the following comparisons:
-10 > 5 # True
-7 == 2 * 3 + 1 # True
-8 != 8 # False
-4 <= 2 + 2 # True
+# #predict the output of the following comparisons:
+# 10 > 5 # True
+# 7 == 2 * 3 + 1 # True
+# 8 != 8 # False
+# 4 <= 2 + 2 # True
 
-# Write 3 examples that result in True and 3 that result in False.
-8 < 10
-7 = 7
-10 != 17
+# # Write 3 examples that result in True and 3 that result in False.
+# 8 < 10
+# 7 = 7
+# 10 != 17
 
-1 != 1
-50 > 100
-20 = 21
+# 1 != 1
+# 50 > 100
+# 20 = 21
 # Create a simple grade-checking condition:
 
 # practice problem :
 # where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+
+grade = int(input("Enter your score: "))
+if grade >= 60:
+    print("You passed the test!")
+else: 
+    print("You did not pass the test.")
